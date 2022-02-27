d1d1ea4ae1d96ffb7fa5c3b9c1776f47f163d7ff
shahafShuhamy <shahaf761@gmail.com>
2022-02-27 21:10:54 +0200

    draggable circle example

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 addEventListener('load', () => {
-    const canvas = window.getElementById("#canvas");
-    const ctx = canvas.getContext("2d");
-    
+    // const canvas = document.getElementById("canvas");
+    // const ctx = canvas.getContext("2d");
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    let stage = new Konva.Stage({
+      container: 'canvas',
+      width: width,
+      height: height,
+    });
+
+    let layer = new Konva.Layer();
+
+    const circle = new Konva.Circle({
+      x: stage.width() / 2,
+      y: stage.height() / 2,
+      radius: 70,
+      fill: 'red',
+      stroke: 'black',
+      strokeWidth: 4,
+      draggable: true
+    });
+
+    circle.on('mouseover', function () {
+        document.body.style.cursor = 'pointer';
+      });
+
+    circle.on('mouseout', function () {
+        document.body.style.cursor = 'default';
+      });
+
+
+    // add the shape to the layer
+    layer.add(circle);
+
+    // add the layer to the stage
+    stage.add(layer);
 });
